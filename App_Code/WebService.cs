774601b8519d1ef2bfd27f75ff45ee6a44fa9e85
Mohit Jain <mohit4u22@gmail.com>
2014-06-15 21:25:07 -0500

    Isa Site Initial Commit By Mohit

---
+++ App_Code/WebService.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+    public string HelloWorld()
+    {
+
+        JavaScriptSerializer js = new JavaScriptSerializer();// Use this when formatting the data as JSON
+        return js.Serialize("Hello World");
+        // return "Hello World";
+    }
+
+    [WebMethod]
+    [ScriptMethod(UseHttpGet = false, ResponseFormat = ResponseFormat.Json)]
+    public string LoginUser(String email, String password)
+    {
+      
+         SqlConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ToString());
+        String retval="";
+
+        try
+        {
+
+            string SQLString = "SELECT * FROM usertable WHERE email='" + email + "'";
+            SqlCommand checkIDTable = new SqlCommand(SQLString, dbConnection);
+            SqlDataReader userdata = checkIDTable.ExecuteReader();
+            if (userdata.Read())
+            {
+                if (userdata["Email"].Equals(email) && userdata["Password"].Equals(password))
+                {
+                    retval = "Success";
+                 
+                }
+                else
+                {
+                    retval = "You have entered Wrong Credentials. Please Try Again!";
+                    //LinkButtonforgot.Visible = true;
+                    //Label1.Text = "You have entered Wrong Credentials. Please Try Again!!!!";
+                }
+            }
+            else
+            {
+                retval = "You have entered Wrong Credentials. Please Try Again!";
+            }
+            userdata.Close();
+            dbConnection.Close();
+        }
+        catch (SqlException exception)
+        {
+            retval = "Some Error";
+        }
+
+
+          JavaScriptSerializer js = new JavaScriptSerializer();
+        return js.Serialize(retval);
+
+            }
+    
+
+
+    [WebMethod]
+    [ScriptMethod(UseHttpGet = false, ResponseFormat = ResponseFormat.Json)]
+    public string SignUpUser(String fname, String lname, String email, String password, String phone, String country, String street, String city, String state, String zip, String securityques, String securityanswer)
+    {
+        //// Database code
+        SqlConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ToString());
+        String retval="";
+        try
+        {
+            dbConnection.Open();
+
+        }
+        catch (SqlException exception)
+        {
+            retval = "There is an Error with the DB, Please try Again!";
+        }
+
+
+                finally
+                {
+                    string sqlstring = "select * from usertable where email='" + email + "';";
+                    SqlCommand checkidtable = new SqlCommand(sqlstring, dbConnection);
+                    SqlDataReader cc = checkidtable.ExecuteReader();
+
+         
+
+                    if (cc.HasRows)
+                    {
+                       retval = "User Email already exists";
+                    }
+
+                    else
+                    {
+                        cc.Close();
+                        
+                        string studentinfo = "insert into usertable values('"
+                            + fname + "', '"
+                            + lname + "', '"
+                            + email + "', '"
+                            + password + "', '"
+                            + phone + "', '"
+                            + country + "', '"
+                            + street + "', '"
+                            + city + "', '"
+                            + state + "', '"
+                            + zip + "', '"
+                            + securityques + "', '"
+                            + securityanswer + "')";
+
+                        SqlCommand sqlcommand1 = new SqlCommand(studentinfo, dbConnection);
+                        sqlcommand1.ExecuteNonQuery();
+                        sqlcommand1.Dispose();
+
+                        dbConnection.Close();
+                        retval="Success";
+
+                        //smtpclient mailclient = new smtpclient();
+                        //mailmessage mail = new mailmessage("mohit4u22@gmail.com", textbox6.text);
+
+                        //string body = "congratulations, you have successfully signed up!! <br>" + "welcome " + textbox2.text + ", " + textbox1.text +
+                        //    "!!!!!<br /> with username : <b>" + textbox3.text + "</b><br /> and password :<b> " + textbox4.text +
+                        //    "</b><br /> confirm password : " + textbox5.text +
+                        //    "<br /> emailid : " + textbox6.text +
+                        //    "<br /> security question : " + textbox7.text +
+                        //    "<br /> security answer : " + textbox8.text +
+                        //    "<br /> categories of interest : " + cat +
+                        //    "<br /> cost: min : " + textbox9.text + " and max :" + textbox10.text +
+                        //    "<br /> size: min : " + textbox11.text + " and max :" + textbox12.text + "<br/>";
+
+                        //string path = server.mappath("images/isulogo.jpg");
+                        //linkedresource logo = new linkedresource(path);
+                        //logo.contentid = "mylogo";
+                        //alternateview altview = alternateview.createalternateviewfromstring("<img src=cid:mylogo/><br />" + body, null, "text/html");
+                        //altview.linkedresources.add(logo);
+
+                        //mail.alternateviews.add(altview);
+
+                        //mail.subject = "registration confirmation";
+                        //mailclient.send(mail);
+                        //response.redirect("registered.aspx");
+                    }
+                
+
+      
+                }
+
+          JavaScriptSerializer js = new JavaScriptSerializer();// Use this when formatting the data as JSON
+        return js.Serialize(retval);
+
+            }
+    
+
+    }
+
---
+++ js/myscripts.js
@@ -0,0 +1,138 @@
+﻿
+var SignupForm;
+$(document).ready(function () {
+
+    SignupForm = $("#frmRegister").validate({
+        rules: {
+            // txtregisterFName: "required",
+            //txtregisterLName: "required"
+            txtregisterFName: {
+                required: true,
+                minlength: 2
+            }
+            //password: {
+            //    required: true,
+            //    minlength: 5
+            //},
+            //confirm_password: {
+            //    required: true,
+            //    minlength: 5,
+            //    equalTo: "#password"
+            //},
+            //email: {
+            //    required: true,
+            //    email: true
+            //},
+            //topic: {
+            //    required: "#newsletter:checked",
+            //    minlength: 2
+            //},
+            //agree: "required"
+        },
+        messages: {
+            //txtregisterFName: "Please enter your firstname",
+            // txtregisterLName: "Please enter your lastname"
+            txtregisterFName: {
+                required: "Please enter a username",
+                minlength: "Your username must consist of at least 2 characters"
+            }
+            //password: {
+            //    required: "Please provide a password",
+            //    minlength: "Your password must be at least 5 characters long"
+            //},
+            //confirm_password: {
+            //    required: "Please provide a password",
+            //    minlength: "Your password must be at least 5 characters long",
+            //    equalTo: "Please enter the same password as above"
+            //},
+            //email: "Please enter a valid email address",
+            //agree: "Please accept our policy"
+        }
+    });
+
+});
+
+
+
+function LoginUser() {
+
+    var val = LoginForm.valid();
+    var email = $('#txtloginEmail').val()
+    var password = $('#txtloginPassword').val()
+
+    $.ajax({
+        type: "Post",
+        url: "../WebService.asmx/LoginUser",
+        data: "{'email': '" + email + "', 'password': '" + password + "'}",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (result) {
+            if (result.d == "Success") {
+                alert("User Logged in Successfully");
+            }
+            else {
+                alert("We're sorry but we are not able to authorize user as this time. <br>" + result.d);
+            }
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.responseText);
+        }
+    });
+}
+
+function RegisterUser() {
+
+
+   var val =  SignupForm.valid();
+    return;
+    alert(isValid);
+    var firstname = $('#txtregisterFName').val();
+    var lastname = $('#txtregisterFName').val();
+    var email = $('#txtregisterFName').val();
+    var confirmemail = $('#txtregisterFName').val();
+    var password = $('#txtregisterFName').val();
+    var confirmpassword = $('#txtregisterFName').val();
+    var phone = $('#txtregisterFName').val();
+    var country = $('#txtregisterFName').val();
+    var street = $('#txtregisterFName').val();
+    var city = $('#txtregisterFName').val();
+    var state = $('#txtregisterFName').val();
+    var zip = $('#txtregisterFName').val();
+    var securityques = $('#txtregisterFName').val();
+    var securityanswer = $('#txtregisterFName').val();
+
+    $.ajax({
+        type: "Post",
+        url: "../WebService.asmx/SignUpUser",
+        data: "{'fname': '" + firstname +
+            "', 'lname': '" + lastname +
+            "', 'email': '" + email +
+            "', 'cemail': '" + confirmemail +
+            "', 'password': '" + password +
+            "', 'cpassword': '" + confirmpassword +
+              "', 'phone': '" + phone +
+             "', 'country': '" + country +
+            "', 'street': '" + street +
+            "', 'city': '" + city +
+            "', 'state': '" + state +
+            "', 'zip': '" + zip +
+            "', 'securityques': '" + securityques +
+            "', 'securityanswer': '" + securityanswer +
+            "'}",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (result) {
+            if (result.d == "Success") {
+                alert("User Created Successfully");
+            }
+            else {
+                alert("We're sorry but we are not able to create the user as this time. <br>" + result.d);
+            }
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("WE're unable to create the user as this time.");
+        }
+    });
+}
